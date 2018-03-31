a9403cf87ab6dd308e0e9f4404b5a651119ecc30
Kristian Sons <kristian.sons@supporting.com>
2018-03-31 09:18:54 +0200

    Cleaned-up svg serializer

--- dist/lib/svg.js
+++ dist/lib/svg.js
@@ -6,7 +6,7 @@ const path = require("path");
 const pd = require("pretty-data");
 const __1 = require("..");
 const BBox_1 = require("./BBox");
-const DXF_FILE_PATH = path.join(__dirname, '..', '..', 'test', 'data', 'dxf', 'GMG1016S19_AAMA.DXF');
+const DXF_FILE_PATH = path.join(__dirname, '..', '..', 'test', 'data', 'dxf', 'SS17_M POLO REG_AAMA.DXF');
 const fileStream = fs.createReadStream(DXF_FILE_PATH, { encoding: 'utf8' });
 function roundToTwo(num) {
     return +(Math.round(num + 'e+2') + 'e-2');
@@ -25,7 +25,7 @@ function generatePointsFromShape(shape, vertices, bbox, size) {
         const vidx = shape.vertices[i];
         const x = vertices[vidx * 2];
         const y = -vertices[vidx * 2 + 1];
-        const circleSVG = `<circle class="size${size}" cx="${roundToTwo(x)}" cy="${roundToTwo(y)}" r="2" fill="red" vector-effect="non-scaling-stroke" />`;
+        const circleSVG = `<circle class="size${size}" cx="${roundToTwo(x)}" cy="${roundToTwo(y)}" r="10" fill="red" vector-effect="non-scaling-stroke" />`;
         circles.push(circleSVG);
         bbox.addToBox(x, y);
         i++;
@@ -85,14 +85,17 @@ parser.parseStream(fileStream, (err, res) => {
     let layerStr = '';
     let layerCount = 0;
     data.pieces.forEach(piece => {
-        const bounderies = [];
-        let internalShapes = [];
-        let turnPoints = [];
-        let curvePoints = [];
-        let grainLines = [];
-        let notches = [];
-        let gradeReferences = [];
-        let mirrorLines = [];
+        const layers = {
+            bounderies: { svg: [], name: 'bounderies' },
+            internalShapes: { svg: [], name: 'internal' },
+            turnPoints: { svg: [], name: 'turn points' },
+            curvePoints: { svg: [], name: 'curve points' },
+            grainLines: { svg: [], name: 'grainLines' },
+            notches: { svg: [], name: 'notches' },
+            gradeReferences: { svg: [], name: 'gradeReference' },
+            mirrorLines: { svg: [], name: 'mirrorLines' },
+            drillHoles: { svg: [], name: 'drillHoles' }
+        };
         data.sizes.forEach(size => {
             const isBaseSize = size === baseSize;
             const id = piece.name + '-' + size;
@@ -103,72 +106,46 @@ parser.parseStream(fileStream, (err, res) => {
                 const sizePath = `<path id="path-${id}" class="size${size}" d="${d}" fill="${fill}" stroke="${color}" vector-effect="non-scaling-stroke"/>`;
                 if (isBaseSize) {
                     // prepend the baseSize path as it is filled and should be on the bottom of the size stack
-                    bounderies.unshift(sizePath);
+                    layers.bounderies.svg.unshift(sizePath);
                 }
                 else {
-                    bounderies.push(sizePath);
+                    layers.bounderies.svg.push(sizePath);
                 }
             }
             const di = generateSegmentsFromShape(piece.internalShapes[size], data.vertices, bbox);
-            internalShapes = internalShapes.concat(di.map((dStr, idx) => {
+            layers.internalShapes.svg = layers.internalShapes.svg.concat(di.map((dStr, idx) => {
                 return `<path id="internal-${id}-${idx}" class="size${size} internal" d="${dStr}" fill="none" stroke="blue" vector-effect="non-scaling-stroke"/>`;
             }));
             const tp = generatePointsFromShape(piece.turnPoints[size], data.vertices, bbox, size);
-            turnPoints = turnPoints.concat(tp);
+            layers.turnPoints.svg = layers.turnPoints.svg.concat(tp);
+            const dh = generatePointsFromShape(piece.drillHoles[size], data.vertices, bbox, size);
+            layers.drillHoles.svg = layers.drillHoles.svg.concat(dh);
             const cp = generatePointsFromShape(piece.curvePoints[size], data.vertices, bbox, size);
-            curvePoints = curvePoints.concat(cp);
+            layers.curvePoints.svg = layers.curvePoints.svg.concat(cp);
             const no = generatePointsFromShape(piece.notches[size], data.vertices, bbox, size);
-            notches = notches.concat(no);
+            layers.notches.svg = layers.notches.svg.concat(no);
             const gl = generateSegmentsFromShape(piece.grainLines[size], data.vertices, bbox);
-            grainLines = grainLines.concat(gl.map((dStr, idx) => {
+            layers.grainLines.svg = layers.grainLines.svg.concat(gl.map((dStr, idx) => {
                 return `<path id="grainline-${id}-${idx}" class="size${size} internal" d="${dStr}" fill="none" stroke="black" vector-effect="non-scaling-stroke" />`;
             }));
             const ml = generateSegmentsFromShape(piece.mirrorLines[size], data.vertices, bbox);
-            mirrorLines = mirrorLines.concat(ml.map((dStr, idx) => {
+            layers.mirrorLines.svg = layers.mirrorLines.svg.concat(ml.map((dStr, idx) => {
                 return `<path id="grainline-${id}-${idx}" class="size${size} internal" d="${dStr}" fill="none" stroke="black" vector-effect="non-scaling-stroke" />`;
             }));
             const grl = generateSegmentsFromShape(piece.gradeReferences[size], data.vertices, bbox);
-            gradeReferences = gradeReferences.concat(grl.map((dStr, idx) => {
+            layers.gradeReferences.svg = layers.gradeReferences.svg.concat(grl.map((dStr, idx) => {
                 return `<path id="grainline-${id}-${idx}" class="size${size} internal" d="${dStr}" fill="none" stroke="green" vector-effect="non-scaling-stroke" />`;
             }));
         });
         layerStr += `<g id="layer${layerCount++}" inkscape:label="${piece.name}" inkscape:groupmode="layer">`;
-        layerStr += bounderies.join();
-        if (internalShapes.length) {
-            layerStr += `<g id="layer${layerCount++}" inkscape:label="${piece.name} internal" inkscape:groupmode="layer">`;
-            layerStr += internalShapes.join();
-            layerStr += '</g>';
-        }
-        if (turnPoints.length) {
-            layerStr += `<g id="layer${layerCount++}" inkscape:label="${piece.name} turn points" inkscape:groupmode="layer">`;
-            layerStr += turnPoints.join();
-            layerStr += '</g>';
-        }
-        if (curvePoints.length) {
-            layerStr += `<g id="layer${layerCount++}" inkscape:label="${piece.name} curve points" inkscape:groupmode="layer">`;
-            layerStr += curvePoints.join();
-            layerStr += '</g>';
-        }
-        if (grainLines.length) {
-            layerStr += `<g id="layer${layerCount++}" inkscape:label="${piece.name} grainLines" inkscape:groupmode="layer">`;
-            layerStr += grainLines.join();
-            layerStr += '</g>';
-        }
-        if (notches.length) {
-            layerStr += `<g id="layer${layerCount++}" inkscape:label="${piece.name} notches" inkscape:groupmode="layer">`;
-            layerStr += notches.join();
-            layerStr += '</g>';
-        }
-        if (gradeReferences.length) {
-            layerStr += `<g id="layer${layerCount++}" inkscape:label="${piece.name} gradeReference" inkscape:groupmode="layer">`;
-            layerStr += gradeReferences.join();
-            layerStr += '</g>';
-        }
-        if (mirrorLines.length) {
-            layerStr += `<g id="layer${layerCount++}" inkscape:label="${piece.name} mirrorLines" inkscape:groupmode="layer">`;
-            layerStr += mirrorLines.join();
-            layerStr += '</g>';
-        }
+        Object.keys(layers).forEach(layerName => {
+            const layer = layers[layerName];
+            if (layer.svg.length) {
+                layerStr += `<g id="layer${layerCount++}" inkscape:label="${piece.name} ${layer.name}" inkscape:groupmode="layer">`;
+                layerStr += layer.svg.join();
+                layerStr += '</g>';
+            }
+        });
         layerStr += '</g>';
     });
     let svgString = '<?xml version="1.0"?>';
